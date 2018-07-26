ae250cd376aecf66d1f31880ff867e34d8d271f4
HeebaShaikh <heeba126@gmail.com>
2018-07-26 00:06:09 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -13,7 +13,7 @@ puts "MY FAVORITE WORD IS #{"food".upcase}
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
-phrase = "IM NOT SHOUTING"
+phrase = "W
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
